cdc0ca9018675763af74c5ea03c1e0cf7b47b661
amasdeogratias <deogratiasprog@gmail.com>
2022-12-14 16:07:09 +0300

    added routes/paths for user register and login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix'=> 'auth'], function(){
+    Route::post('/register', [\App\Http\Controllers\AuthController::class,'register'])->name('register');
+    Route::post('/login', [\App\Http\Controllers\AuthController::class,'login'])->name('login');
 });
