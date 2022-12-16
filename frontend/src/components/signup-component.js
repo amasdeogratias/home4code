ffe2a7c8974049a05a3b66bd342820fe584cdc40
amasdeogratias <deogratiasprog@gmail.com>
2022-12-16 15:22:52 +0300

    changed login component to functional component

--- frontend/src/components/signup-component.js
+++ frontend/src/components/signup-component.js
@@ -3,63 +3,63 @@ import React, { useState } from 'react'
 const SignUp = () => {
   
     // handle form submit
-    const [first_name, setFirstName] = useState('');
-    const [last_name, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
-    
-    //create submit function
-    const submit = (e) => {
-      console.log({
-        first_name, last_name, email, password
-      })
-    }
-    return (
-      <form onSubmit={submit}>
-        <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-            onChange={e=>setFirstName(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" 
-          onChange={e=> setLastName(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            onChange={e => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            onChange={e => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/sign-in">sign in?</a>
-        </p>
-      </form>
-    )
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  
+  //create submit function
+  const submit = (e) => {
+    console.log({
+      first_name, last_name, email, password
+    })
+  }
+  return (
+    <form onSubmit={submit}>
+      <h3>Sign Up</h3>
+      <div className="mb-3">
+        <label>First name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="First name"
+          onChange={e=>setFirstName(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Last name</label>
+        <input type="text" className="form-control" placeholder="Last name" 
+        onChange={e=> setLastName(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Already registered <a href="/sign-in">sign in?</a>
+      </p>
+    </form>
+  )
   
 }
 
