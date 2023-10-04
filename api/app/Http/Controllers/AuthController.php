695df6e3878ba6b27a30d3a1e5dc51cc81906bb7
fixnetcodes <deogratiasprog@gmail.com>
2023-10-04 16:51:02 +0300

    added register route and method in controller

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Auth;
 use DB;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Validator;
 
 class AuthController extends Controller
 {
@@ -32,5 +33,40 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'Invalid email or password',
         ], 401);
+    }
+
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:55',
+            'email' => 'required|unique:users,email|min:5|max:60',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        if($validator->fails()){
+            return response(['Validation Error'=>$validator->errors()->toJson()], 422);
+        }
+
+        try{
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $token = $user->createToken('passport')->accessToken;
+
+            return response([
+                'message' => 'Registration successfully...',
+                'token' => $token,
+                'user' => $user
+            ], 200);
+
+        }catch(Exception $e){
+            return response([
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -20,3 +20,4 @@ use App\Http\Controllers\AuthController;
 // });
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
