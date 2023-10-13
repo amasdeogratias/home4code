98c174303482188460c193c421e032114247738f
fixnetcodes <deogratiasprog@gmail.com>
2023-10-13 12:16:03 +0300

    added react authentication using laravel passport

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    message: ''
+  }
+  
+  formSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/login', {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then((response) => {
+      console.log(response);
+      localStorage.setItem('token', response.data.token);
+      this.setState({
+        loggedIn: true
+      })
+      this.props.setUser(response.data.user)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    
+  }
   render() {
+    
+    //if logged in redirect to profile
+    if(this.state.loggedIn){
+      return window.location.href='/profile'
+    }
+    
     return (
       <div>
         <div className="row mt-4">
             <div className='jumbotron col-lg-4 offset-lg-4'>
                 <h3 className='text-center'>Login</h3>
-                <form>
+                <form onSubmit={this.formSubmit}>
                     <div className="mb-3">
                         <label>Email address</label>
                         <input
@@ -16,6 +48,9 @@ class Login extends Component {
                             name="email"
                             className="form-control"
                             placeholder="Enter email"
+                            required
+                            autoComplete='off'
+                            onChange={(e)=> {this.setState({email:e.target.value})}}
                         
                         />
                     </div>
@@ -26,6 +61,8 @@ class Login extends Component {
                             name="password"
                             className="form-control"
                             placeholder="Enter password"
+                            required
+                            onChange={(e)=> {this.setState({password:e.target.value})}}
                         
                         />
                     </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import axios from 'axios'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.baseURL = 'http://127.0.0.1:8080/api';
+axios.defaults.headers.common['Authorization'] = 'Bearer '+ localStorage.getItem('token')
 root.render(
   <React.StrictMode>
     <App />
