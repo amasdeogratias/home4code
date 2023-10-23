6d7963a7242290afcaee047d2c7688bfc6b58be6
fixnetcodes <deogratiasprog@gmail.com>
2023-10-23 16:22:35 +0300

    added function for creating new task

--- api/app/Http/Controllers/TaskController.php
+++ api/app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Validator;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -11,5 +12,41 @@ class TaskController extends Controller
     {
 
         return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'title' => 'required|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+        ]);
+
+        if($validator->fails()){
+            return response(['Validation Error' => $validator->errors()->toJson()], 422);
+        }
+
+        try {
+
+            $task = Task::create([
+                'user_id' => $request->user_id,
+                'title' => $request->title,
+                'description' => $request->description,
+                'priority' => $request->priority,
+                'start_date' => $request->start_date,
+                'duration' => $request->duration,
+                'end_date' => $request->end_date,
+            ]);
+
+            return response([
+                'message' => 'Task created successfully...'
+            ],200);
+
+        } catch(\Exception $e) {
+            return response([
+                'message' => $e->getMessage()
+            ],500);
+        }
     }
 }
