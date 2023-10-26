aecd9585e55979f3ac3d84a0d1c560d12e1df30b
fixnetcodes <deogratiasprog@gmail.com>
2023-10-26 11:54:00 +0300

    added task update functionality

--- api/app/Http/Controllers/TaskController.php
+++ api/app/Http/Controllers/TaskController.php
@@ -58,4 +58,44 @@ class TaskController extends Controller
             ],500);
         }
     }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'title' => 'required|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+        ]);
+
+        if($validator->fails()){
+            return response(['Validation Error' => $validator->errors()->toJson()], 422);
+        }
+
+        $task = Task::find($id);
+        try {
+            if(!$task){
+                return response([
+                    'message' => "This task is not in the our records"
+                ],500);
+            }
+            $task->user_id = $request->user_id;
+            $task->title = $request->title;
+            $task->description = $request->description;
+            $task->priority = $request->priority;
+            $task->start_date = $request->start_date;
+            $task->duration = $request->duration;
+            $task->end_date = $request->end_date;
+            $task->save();
+
+            return response([
+                'message' => "Task updated successfully"
+            ],200);
+
+        }catch(\Exception $e){
+            return response([
+                'message' => $e->getMessage()
+            ],500);
+        }
+    }
 }
