cb9b5afb2f48395b473cd431d07ddab2806d438a
fixnetcodes <deogratiasprog@gmail.com>
2023-11-13 16:07:15 +0300

    Added cards in the dashboard

--- frontend/src/common/Home.js
+++ frontend/src/common/Home.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react'
 import Sidebar from '../components/Sidebar'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 class Home extends Component {
+  state = {
+    tasks:[],
+    users:[]
+  }
+  
+  componentDidMount() {
+    this.fetchTasks();
+  }
+  fetchTasks = () => {
+    axios.get('/tasks')
+    .then(response => {
+        this.setState({ tasks: response.data });
+    })
+    .catch(error => {
+        console.error('Error fetching tasks:', error);
+    });
+};
   render() {
     return (
       <div className='container-fluid page-body-wrapper'>
         <Sidebar />
-        <h1>Home Page</h1>
-        
+        <div className="main-panel">
+          <div className="content-wrapper">
+            <div className="row">
+                <div className="col-md-3 mb-4 stretch-card transparent">
+                    <div className="card card-tale">
+                        <Link to={"/tasks"} style={{textDecoration: "none"}}>
+                            <div className="card-body text-white">
+                                <p className="mb-4">Total Users</p>
+                                <p className="fs-30 mb-2">5</p>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+                <div className="col-md-3 mb-4 stretch-card transparent">
+                    <div className="card card-dark-blue">
+                        <Link to={"/tasks"} style={{textDecoration: "none"}}>
+                            <div className="card-body text-white">
+                                <p className="mb-4">Total Tasks Present</p>
+                                <p className="fs-30 mb-2">{this.state.tasks.length}</p>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
